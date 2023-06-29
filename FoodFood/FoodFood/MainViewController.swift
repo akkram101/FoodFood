79e110dc74440ff60dcd64ef0be6a2528a575f5d
Akkram <akkram101@gmail.com>
2023-06-29 10:13:12 +0800

    new repo test

--- FoodFood/FoodFood/MainViewController.swift
+++ FoodFood/FoodFood/MainViewController.swift
@@ -12,7 +12,7 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("testtest")
+        print("new repo test")
         self.view.backgroundColor = .red
         // Do any additional setup after loading the view.
     }
